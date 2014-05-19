3604c96ad2a7497f7a92f25d8342b8b3956aba6b
Abner-BesEye <abner.huang@beseye.com>
2014-05-19 08:58:44 +0800

    Add more log

--- BeseyeApp/jni/streaming/source/beseyeplayer.cpp
+++ BeseyeApp/jni/streaming/source/beseyeplayer.cpp
@@ -2554,6 +2554,14 @@ void CBeseyePlayer::invokeRtmpStreamMethodCallback(const AVal* method, const AVa
 				av_log(NULL, AV_LOG_INFO, "invokeRtmpCallback(), desc:%s", desc.av_val);
 			}
 		}
+	}else{
+		av_log(NULL, AV_LOG_INFO, "invokeRtmpCallback()2, non-handled content:[%s]", (NULL != content)?content->av_val:"");
+		AMFObject* obj = (AMFObject*)extra;
+		AVal desc;
+		if(obj){
+			AMFProp_GetString(AMF_GetProp(obj, &av_details, -1), &desc);
+			av_log(NULL, AV_LOG_INFO, "invokeRtmpCallback(), desc:%s", desc.av_val);
+		}
 	}
 }
 
