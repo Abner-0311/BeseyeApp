d135837f9b638d2beaafd65088ef109c1c893a5d
Abner-BesEye <abner.huang@beseye.com>
2014-06-01 06:30:41 +0800

    Update version

--- BeseyeApp/src/com/app/beseye/util/BeseyeConfig.java
+++ BeseyeApp/src/com/app/beseye/util/BeseyeConfig.java
@@ -20,7 +20,7 @@ public class BeseyeConfig {
 	
 	static public final boolean REDDOT_DEMO = false;//deprecated
 	
-	static public final boolean COMPUTEX_DEMO = false;
+	static public final boolean COMPUTEX_DEMO = true;
 	static public boolean COMPUTEX_PAIRING = false;
 	
 	static public final String RELAY_AP_SSID = "raylios WiFi";
