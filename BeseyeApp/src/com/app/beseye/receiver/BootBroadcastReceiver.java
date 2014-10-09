8540ce3fa6055959c6c67695e0b6e5924f69ce09
Abner-BesEye <abner.huang@beseye.com>
2014-10-09 15:01:50 +0800

    Add boot broadcast receiver

---
+++ BeseyeApp/src/com/app/beseye/receiver/BootBroadcastReceiver.java
@@ -0,0 +1,22 @@
+package com.app.beseye.receiver;
+
+import static com.app.beseye.util.BeseyeConfig.TAG;
+
+import com.app.beseye.service.BeseyeNotificationService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		String action = intent.getAction();
+		if(action.equals(Intent.ACTION_BOOT_COMPLETED)) {
+			Log.i(TAG, "*****************BootBroadcastReceiver::onReceive(), ACTION_BOOT_COMPLETED invoked");
+			context.startService(new Intent(context,BeseyeNotificationService.class));
+		}
+	}
+}
